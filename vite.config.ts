410ff04b086b6952ba44b5c2c81faf46acfd9843
Catherine Dolgushina <dolgushinacd@icloud.com>
2025-04-23 09:47:46 +0200

    Add favicon assets and update HTML title; configure static copy plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import svgr from 'vite-plugin-svgr';
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 export default defineConfig({
   plugins: [
@@ -23,5 +24,13 @@ export default defineConfig({
         },
       },
     }),
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'src/assets/favicon/*',
+          dest: 'assets/favicon',
+        }
+      ]
+    })
   ],
 });
